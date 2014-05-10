3a86e044716b2798d469b264107165707cc83092
Andrew Shang <andrew.freeman123@gmail.com>
2014-05-10 14:18:46 -0400

    ooh lalala

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello world");
-        System.out.println("ooh lalal ");
+        System.out.println("ooh lalala");
     }
 }
